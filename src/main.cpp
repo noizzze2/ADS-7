ad84f78564b9abb66ce2645d777e5349bdefff5b
noizzze2 <noizzze20@gmail.com>
2026-05-26 18:06:35 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,46 @@
 // Copyright 2022 NNTU-CS
+#include <cstdlib>
+#include <ctime>
 #include <iostream>
 #include "train.h"
 
 int main() {
-  Train train;
-  int count = 60; // кол-во вагонов
+    std::srand(static_cast<unsigned int>(std::time(nullptr)));
 
-  while (count--)
-    train.addCar(false);
+    std::cout << "n\tfalse\ttrue\trandom\n";
 
-  std::cout << train.getLength() << std::endl;
-  std::cout << train.getOpCount() << std::endl;
-  return 0;
+    for (int n = 2; n <= 100; ++n) {
+        int resFalse, resTrue, resRand;
+
+        {
+            Train train;
+            for (int i = 0; i < n; ++i) {
+                train.addCar(false);
+            }
+            train.getLength();
+            resFalse = train.getOpCount();
+        }
+
+        {
+            Train train;
+            for (int i = 0; i < n; ++i) {
+                train.addCar(true);
+            }
+            train.getLength();
+            resTrue = train.getOpCount();
+        }
+
+        {
+            Train train;
+            for (int i = 0; i < n; ++i) {
+                train.addCar(std::rand() % 2);
+            }
+            train.getLength();
+            resRand = train.getOpCount();
+        }
+
+        std::cout << n << "\t" << resFalse << "\t" << resTrue << "\t" << resRand << "\n";
+    }
+
+    return 0;
 }
